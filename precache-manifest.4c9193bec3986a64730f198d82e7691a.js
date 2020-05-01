e57ebae76389c5ce8ad5d8a4fd26e93138fc9545
PratimaDumpala <62508057+PratimaDumpala@users.noreply.github.com>
2020-05-01 15:23:53 +0530

    Updates

---
+++ precache-manifest.4c9193bec3986a64730f198d82e7691a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6b0b481020e42ee64f5b7c2cfe4eb6e",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "3b5bd50610e8c660939c",
+    "url": "/monsters/static/css/main.350b81fb.chunk.css"
+  },
+  {
+    "revision": "9f834d1fc4df12fb31cd",
+    "url": "/monsters/static/js/2.308474c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.308474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b5bd50610e8c660939c",
+    "url": "/monsters/static/js/main.d853b9e0.chunk.js"
+  },
+  {
+    "revision": "64e2ee1a6a4fdc686046",
+    "url": "/monsters/static/js/runtime-main.e75998aa.js"
+  }
+]);
